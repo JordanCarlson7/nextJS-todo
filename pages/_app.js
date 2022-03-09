d484d9c9f3babc966e5ea7df8f0a1db14bc912a7
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2022-03-08 18:26:55 -0700

    accidently deleted export

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,8 @@ import { SessionProvider } from "next-auth/react";
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </SessionProvider>
   );
-  }
+}
+export default MyApp;
