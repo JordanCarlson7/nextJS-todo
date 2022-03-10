6b993329dd8dd5d7e011ebad5d1723974d0875d6
ntduncan <nateduncan0@.com>
2022-03-09 22:56:22 -0700

    added css for habit items. useState isn't working great

--- pages/habits/HabitList.js
+++ pages/habits/HabitList.js
@@ -33,13 +33,14 @@ const HabitList = ({ habits }) => {
   
   return (
     <Fragment>
+      <h2>My Todo List</h2>
       <ul className={classes.list}>
         {habits.map((item, index) => {
           return <HabitItem item={item} key={index}></HabitItem>;
         })}
       </ul>
       <form>
-        
+        <h1>Add A new Task</h1>
       </form>
     </Fragment>
   );
