1b9c6d6c203612abe3c042d648ba2e9650aa7432
ntduncan <nateduncan0@.com>
2022-03-12 19:30:07 -0700

    get/post route for individual habits

---
+++ pages/api/DBaccess/get-habit.js
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/router'
+import connectDB from '../../../middleware/mongodb';
+import Habit from '../../../models/habit';
+
+
+const handler = async (req, res, next) => {
+    const id = req.body.id
+    console.log(id)
+    
+    Habit.findById(id)
+    .then(habit => {
+      console.log(habit);
+      res.json(habits);
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      res.json(error.message);
+    });
+}
+
+export default connectDB(handler);
