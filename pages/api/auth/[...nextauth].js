dbdfb9f871195f098a6b3ee2b903caedd420392f
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2022-03-12 22:56:31 -0700

    clean up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,10 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-// const obj = {
-//   "GOOGLE_CLIENT_ID": "1085290976684-3elpbg32jigj94do2b7a2cukus4p9ad6.apps.googleusercontent.com",
-//   "GOOGLE_CLIENT_SECRET": "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"
-// }
-
-
 const options = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID, 
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
   ],
